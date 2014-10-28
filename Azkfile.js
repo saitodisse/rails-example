d806c48ae5e08acc64578363ada0347a629b063a
Julio Makdisse Saito <saitodisse@gmail.com>
2014-10-28 02:20:18 -0200

    Merging Azkfile from ruby:2 with gullitmiranda/ruby

--- Azkfile.js
+++ Azkfile.js
@@ -9,30 +9,41 @@ systems({
     depends: [],
     // More images:  http://images.azk.io
     image: "ruby:2.1.3",
-    workdir: "/azk/#{manifest.dir}",
+    // Steps to execute before running instances
     provision: [
-      "bundle install --path vendor/bundler",
+      "bundle install --path /azk/bundler",
+      "bundle exec rake db:create",
+      "bundle exec rake db:migrate",
     ],
-    // command: "rails server --port $HTTP_PORT",
-    command: [
-      "bundle install --path vendor/bundler",
-      "bundle exec rackup config.ru --port $HTTP_PORT"
-    ].join(';'),
+    workdir: "/azk/#{manifest.dir}",
+    command: "bundle install --path /azk/bundler; bundle exec rails server --pid /tmp/rails.pid --port $HTTP_PORT",
+    shell: "/bin/bash",
+    // not expect application response
+    wait: { retry: 3, timeout: 1000 },
+    scalable: { "default": 2 },
+    // Mounts folders to assigned paths
     mounts: {
-      '/azk/#{manifest.dir}': path("."),
+      // equivalent mount_folders
+      '/azk/#{manifest.dir}'  : path('.'),
+      '/www/gems'             : path('../gems', { required: false }),
+      '/azk/bundler'          : persistent('bundler'), // Volume nomed
     },
     http: {
-      // rails-example.azk.dev
-      hostname: "#{system.name}.#{azk.default_domain}"
+      // aircrm.azk.dev
+      domains: [ "#{system.name}.#{azk.default_domain}" ]
     },
     envs: {
       // set instances variables
-      RUBY_ENV: "dev",
-      // GEM_HOME: "/azk/#{manifest.dir}/vendor/bundler/ruby/2.1.0",
-      // BUNDLE_APP_CONFIG: "/azk/#{manifest.dir}/vendor/bundler",
-      // BUNDLE_BIN_PATH: "/azk/#{manifest.dir}/vendor/bundler/ruby/2.1.0/bin",
-      // GEM_PATH: "/azk/#{manifest.dir}/vendor/bundler/ruby/2.1.0/gems",
+      RUBY_ENV: "development",
+      RACK_ENV: "development",
+      RAILS_ENV: "development",
+      ENABLE_AZK: true,
+      HOST: "#{system.name}.#{azk.default_domain}",
     },
+    export_envs: {
+      HTTP_PORT: "#{azk.default_domain}:#{net.port.http}",
+      HTTPS_PORT: "#{azk.default_domain}:#{net.port.http}"
+    }
   },
 });
 
