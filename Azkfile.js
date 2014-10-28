445e3d87243a3a7b4ae022284e04d18b2952736e
Julio Makdisse Saito <saitodisse@gmail.com>
2014-10-28 15:20:56 -0200

    Rails docker image is used, so the user can do a rails new no azk shell

--- Azkfile.js
+++ Azkfile.js
@@ -8,40 +8,34 @@ systems({
     // Dependent systems
     depends: [],
     // More images:  http://images.azk.io
-    image: "ruby:2.1.3",
+    image: "rails:4",
     // Steps to execute before running instances
     provision: [
       "bundle install --path /azk/bundler",
       "bundle exec rake db:create",
       "bundle exec rake db:migrate",
     ],
-    workdir: "/azk/#{manifest.dir}",
-    command: "bundle install --path /azk/bundler; bundle exec rails server --pid /tmp/rails.pid --port $HTTP_PORT",
-    shell: "/bin/bash",
+    workdir : "/azk/#{manifest.dir}",
+    command : "bundle exec rails server --pid /tmp/rails.pid --port $HTTP_PORT",
+    shell   : "/bin/bash",
     // not expect application response
-    wait: { retry: 3, timeout: 1000 },
+    wait    : { retry: 3, timeout: 1000 },
     scalable: { "default": 2 },
     // Mounts folders to assigned paths
     mounts: {
-      // equivalent mount_folders
-      '/azk/#{manifest.dir}'  : path('.'),
-      '/www/gems'             : path('../gems', { required: false }),
-      '/azk/bundler'          : persistent('bundler'), // Volume nomed
+      '/azk/#{manifest.dir}': path('.'),
+      '/azk/bundler'        : persistent('bundler'),
     },
     http: {
-      // aircrm.azk.dev
       domains: [ "#{system.name}.#{azk.default_domain}" ]
     },
     envs: {
       // set instances variables
-      RUBY_ENV: "development",
-      RACK_ENV: "development",
-      RAILS_ENV: "development",
-      ENABLE_AZK: true,
-      HOST: "#{system.name}.#{azk.default_domain}",
+      RUBY_ENV : "development",
+      BUNDLE_APP_CONFIG : "/azk/bundler",
     },
     export_envs: {
-      HTTP_PORT: "#{azk.default_domain}:#{net.port.http}",
+      HTTP_PORT : "#{azk.default_domain}:#{net.port.http}",
       HTTPS_PORT: "#{azk.default_domain}:#{net.port.http}"
     }
   },
